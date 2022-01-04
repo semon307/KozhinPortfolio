6ae850475fbb22413efc0a14165ffae6c3552760
Semen Kozhin <sa.kozh@yandex.ru>
2022-01-04 23:02:17 +0100

    update, 4.01.2022

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,31 +1,31 @@
 import s from "./Main.module.css"
 import common from "../Common/Styles/CommonStyles.module.css"
-import Particles from "react-tsparticles"
+// import Particles from "react-tsparticles"
 import ReactTypingEffect from 'react-typing-effect';
 import myPhoto from "./../Assets/Images/Semen.jpg"
 import Tilt from 'react-parallax-tilt';
 
 
-const particlesOptions = {
-    particles: {
-        number: {
-            value: 50,
-            density: {
-                enable: true,
-                value_area: 800
-            }
-        }
-    }
-};
+// const particlesOptions = {
+//     particles: {
+//         number: {
+//             value: 50,
+//             density: {
+//                 enable: true,
+//                 value_area: 800
+//             }
+//         }
+//     }
+// };
 export const Main = () => {
 
     return (
         <div id={"Main"} className={s.mainBlock}  >
             <div className={s.particlesDiv} style={{ position: "relative" }}>
-            <Particles
-                id={"particles"}
-                className={s.particles}
-                params={particlesOptions}/>
+            {/*<Particles*/}
+            {/*    id={"particles"}*/}
+            {/*    className={s.particles}*/}
+            {/*    params={particlesOptions}/>*/}
             </div>
             <div className={common.container}>
 
