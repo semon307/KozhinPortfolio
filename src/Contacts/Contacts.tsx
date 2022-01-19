584abe2d64a3f8f4fe763a173f5c8496e5f5ec0d
Semen Kozhin <sa.kozh@yandex.ru>
2022-01-19 11:04:41 +0100

    update, 19.01.2022

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -7,11 +7,13 @@ import emailjs from 'emailjs-com';
 type EmailStatusType = "none" | "succeded" | "failed"
 export const Contacts = () => {
     const [emailStatus, setEmailStatus] = useState<EmailStatusType>("none")
-
+    const [disabled, setDisabled] = useState(true)
     const form = useRef();
 
     const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        console.log(data)
 
         // @ts-ignore
         emailjs.sendForm('service_f40drjk', 'template_2u6n4qr', e.target, 'user_WVLxYyOZjV4oINEwJY090')
@@ -35,11 +37,11 @@ export const Contacts = () => {
                         <div className={s.attention}>{emailStatus === "succeded" && "I appreciate your attention and I will contact You as soon as it is possible."}</div>
                         <div className={s.attentionError}>{emailStatus === "failed" && "There are some technical troubles with your request. Please, choose another way to contact me."}</div>
 
-                        <form onSubmit={sendEmail} className={s.form}>
-                            <input className={s.input} placeholder={"Your name"} name={"email"}/>
-                            <input className={s.input} placeholder={"Your E-mail"} name={"name"}/>
-                            <textarea className={s.textArea} placeholder={"Type text here"} name={"message"}/>
-                            <input className={buttonStyle.button} type={"submit"} value={"Send"}/>
+                        <form id={"form"} name={"form"} onSubmit={sendEmail} className={s.form}>
+                            <input id={"email"} className={s.input} placeholder={"Your name"} name={"email"} required={true}/>
+                            <input id={"name"} className={s.input} placeholder={"Your E-mail"} name={"name"} required/>
+                            <textarea id={"message"} className={s.textArea} placeholder={"Type text here"} name={"message"} required/>
+                            <input id={"submit"} className={buttonStyle.button} type={"submit"} value={"Send"}/>
                         </form>
                 </div>
             </div>
